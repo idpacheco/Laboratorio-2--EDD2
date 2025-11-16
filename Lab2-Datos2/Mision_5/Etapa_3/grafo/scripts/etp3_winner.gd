0ef5ae1c45cfcef1b8796fb9062aff6a6d2f92cc
Danna <galavisd@uninorte.edu.co>
2025-11-16 12:08:35 -0500

    Etapa4 y etapa3 conectadas

--- Lab2-Datos2/Mision_5/Etapa_3/grafo/scripts/etp3_winner.gd
+++ Lab2-Datos2/Mision_5/Etapa_3/grafo/scripts/etp3_winner.gd
@@ -2,6 +2,6 @@ extends Control
 
 
 func _on_next_to__pressed() -> void:
-	#SceneTransitions.change_scene_to_file("res://Mision_3/Mision 3/scenes/winner.tscn")
+	SceneTransitions.change_scene_to_file("res://Mision_5/Etapa_4/Grafo/Scenes/Contexto.tscn")
 	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
 	AudioManager.SFXPlayer.play()
